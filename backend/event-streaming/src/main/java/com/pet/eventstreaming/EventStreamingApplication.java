8ad612241ca36583c0e93f67e56cbc7637c9ef6c
ibrus <brus.andreevich@gmail.com>
2025-04-07 13:52:41 +0300

    Initial commit

---
+++ backend/event-streaming/src/main/java/com/pet/eventstreaming/EventStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.pet.eventstreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventStreamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventStreamingApplication.class, args);
+    }
+
+}
---
+++ backend/event-streaming/src/test/java/com/pet/eventstreaming/EventStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.eventstreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventStreamingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
